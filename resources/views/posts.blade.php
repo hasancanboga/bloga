f51a2bde8d1ce839b7a9d1e02b2d7a054f082482
Hasancan Boga <hasancanboga@gmail.com>
2021-05-06 22:27:19 +0300

    part 11 done

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <a href="/posts/my-first-post">
+            <h1>My First Post</h1>
+        </a>
+        <p>
+            asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd
+        </p>
+    </article>
+    <article>
+        <a href="/posts/my-second-post">
+            <h1>My Second Post</h1>
+        </a>
+
+        <p>
+            asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd
+        </p>
+    </article>
+    <article>
+        <a href="/posts/my-third-post">
+            <h1>My Third Post</h1>
+        </a>
+        <p>
+            asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd sdfasdfgasdfgasd asdasdfsadfasdfasd
+        </p>
+    </article>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function ($slug) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
+
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+'); // whereAlpha for only letters. whereAlphaNumeric for letters and numbers.
