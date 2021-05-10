e952a24a8e98b56ae6172dd161692e3463b166b6
Hasancan Boga <hasancanboga@gmail.com>
2021-05-10 20:23:16 +0300

    categories and clockwork implemented

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,17 @@
 {{-- @section('content') --}}
 <x-layout>
     <article>
-        <h1>{{ $post->title }}</h1>
+        <h1>
+            {{ $post->title }}
+        </h1>
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
+
         <div>
-            {!! $post->body !!}
+            <p>
+                {{ $post->body }}
+            </p>
         </div>
     </article>
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +17,23 @@ use Illuminate\Support\Facades\File;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', ['posts' => Post::all()]);
+Route::get('/test', function () {
 });
 
-Route::get('posts/{post}', function ($id) {
-    // Find a post by its slug and pass it to a view called "post"
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::with('category')->get()
+    ]);
+});
+
+Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
-        'post' => Post::findOrFail($id)
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
